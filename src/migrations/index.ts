9dafdb432c532b8323988044d84d216f1a1a2f73
Mohammad <mohamad92ahm@gmail.com>
2025-11-21 09:02:07 +0300

    migration added

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251028_114739 from './20251028_114739';
+import * as migration_20251121_060055 from './20251121_060055';
 
 export const migrations = [
   {
     up: migration_20251028_114739.up,
     down: migration_20251028_114739.down,
-    name: '20251028_114739'
+    name: '20251028_114739',
+  },
+  {
+    up: migration_20251121_060055.up,
+    down: migration_20251121_060055.down,
+    name: '20251121_060055'
   },
 ];
